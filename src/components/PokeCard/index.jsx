392f395e157d5b6178d748cc9696410b2cadc6f4
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-12-05 09:23:18 -0300

    feat: Component pokemon (pokenent)

--- src/components/PokeCard/index.jsx
+++ src/components/PokeCard/index.jsx
@@ -6,17 +6,17 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function PokdeCard() {
+export default function PokdeCard( { name, image } ) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 200 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image={image}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {name}
         </Typography>
         {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PokdeCard from "../components/PokeCard";
 import NavBar from "../components/NavBar";
 import { Container, Grid } from "@mui/material";
 import axios from "axios";
 
 export const Home = () => {
+    const [pokemons, setPokemons ] = useState([]);
+
     useEffect(() =>{
         getPokemons();
-    })
+    }, [])
     
-    constG
-    axios.get("https://p  okeapi.com/api/v2/pokemon?limit=50").then(() => )
+    const getPokemons = () => {
+        var endpoints = []
+        for (var i = 1; i < 50; i++){
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        }
+        
+        var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+
+        // axios
+        // .get("https://pokeapi.co/api/v2/pokemon?Limit=50")
+        // .then((res) => setPokemons(res.data.results))
+        // .catch((err) => console.log(err));
+    };
+
     return (
         <div>
             <NavBar/>
             <Container maxWidth="false">
-                <Grid container>
-                    <Grid item xs={3}>
-                        <PokdeCard/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <PokdeCard/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <PokdeCard/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <PokdeCard/>
-                    </Grid>
+                <Grid container spacing={2}>
+                    {pokemons.map((pokemon, key) =>
+                        <Grid item xs={2} key={key}>
+                            <PokdeCard name={pokemon.data.name} image={pokemon.data.sprites.front_default}/>
+                        </Grid> 
+                    )}
                 </Grid>
                 
             </Container>
