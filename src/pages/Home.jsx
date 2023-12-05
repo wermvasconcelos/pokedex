c381b1f181508860685ed42a44aee0f2de00de02
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-12-05 10:06:40 -0300

    feat: Reponsividade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,14 +25,30 @@ export const Home = () => {
         // .catch((err) => console.log(err));
     };
 
+    const pokemonFilter = ( name ) => {
+        var filteredPokemons = []
+
+        if (name === "") {
+            getPokemons()
+        }
+
+        for (var i in pokemons) {
+            if (pokemons[i].data.name.includes(name)) {
+                filteredPokemons.push(pokemons[i])
+            }
+        }
+
+        setPokemons(filteredPokemons)
+    };
+
     return (
         <div>
-            <NavBar/>
-            <Container maxWidth="false">
+            <NavBar pokemonFilter={pokemonFilter}/>
+            <Container maxWidth="false" align="center">
                 <Grid container spacing={2}>
                     {pokemons.map((pokemon, key) =>
-                        <Grid item xs={2} key={key}>
-                            <PokdeCard name={pokemon.data.name} image={pokemon.data.sprites.front_default}/>
+                        <Grid item xs={12}  sm={6} md={4} lg={2} key={key}>
+                            <PokdeCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types}/>
                         </Grid> 
                     )}
                 </Grid>
