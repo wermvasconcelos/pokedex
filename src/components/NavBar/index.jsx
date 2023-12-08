0d74396f3a63627abf8cf4cccad6d4f70e752f2f
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-12-08 14:33:14 -0300

    feat: criando rotas

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router-dom';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -51,22 +52,25 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function NavBar( { pokemonFilter } ) {
+export default function NavBar({ pokemonFilter, hideSearch }) {
+  const navigate = useNavigate()
   return (
     <Box sx={{ flexGrow: 1, marginBottom: "2em" }}>
       <AppBar position="static" sx={{ backgroundColor: "black" }}>
         <Toolbar>
           <Box display="flex" justifyContent="space-between" width="100%">
-            <Box component="img" src="/assests/image/logo.png" height="3em" />
-            <Search onChange={(e )=> pokemonFilter(e.target.value)}>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Pesquisando..."
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </Search>
+            <Box component="img" src="/assests/image/logo.png" height="3em" sx={{cursor:"pointer"}} onClick={() => navigate("/")}/>
+            {!hideSearch && (
+              <Search onChange={(e) => pokemonFilter(e.target.value)}>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Pesquisando..."
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+            )}
           </Box>
 
         </Toolbar>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+
+export const Profile = () => {
+    return (
+        <>
+        <NavBar hideSearch/>
+        </>
+    )
+}
